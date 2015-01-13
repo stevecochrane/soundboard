1337c41e57ad410fa9d2c949a5bbe43001ac107a
Steve Cochrane <steve@stevecochrane.com>
2015-01-12 23:34:59 -0600

    Removed the audio embed and added a button.

--- src/js/main.js
+++ src/js/main.js
@@ -1,3 +1,13 @@
 var $ = require("jquery");
 
-// make beautiful things here
+$(function() {
+
+    var audioPriceIsRight = window.document.createElement("audio");
+    audioPriceIsRight.setAttribute("src", "audio/price-is-right.mp3");
+
+    $("#button-price-is-right").click(function() {
+        audioPriceIsRight.currentTime = 0;
+        audioPriceIsRight.play();
+    });
+
+});
