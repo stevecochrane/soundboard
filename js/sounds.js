8272c3ea0a2fe64af5a2065b33db03d7aa9ea457
Steve Cochrane <steve@stevecochrane.com>
2016-08-27 22:02:34 -0500

    Added a new sounds.js file for storing the data.

---
+++ js/sounds.js
@@ -0,0 +1,22 @@
+var sounds = [
+    {
+        file: "price-is-right.mp3",
+        id: "price-is-right",
+        label: "Price is Right Fail"
+    },
+    {
+        file: "hearthstone.mp3",
+        id: "hearthstone",
+        label: "&ldquo;Hearthstone&rdquo;"
+    },
+    {
+        file: "jobs-done.mp3",
+        id: "jobs-done",
+        label: "Job&rsquo;s Done"
+    },
+    {
+        file: "circus-theme.mp3",
+        id: "circus-theme",
+        label: "Circus Theme"
+    }
+];
